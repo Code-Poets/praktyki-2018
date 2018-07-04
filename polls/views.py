8fcf348b227958ffb317b7bac35d96d0fc4c2217
Your Name <you@example.com>
2018-07-04 14:32:00 +0200

    Add template
    Add template for index view (using shortcut render()).

--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 
-
+#view index use template 
+#shortcut render
 def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
