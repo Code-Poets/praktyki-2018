0eba5cda990d3c82ed94d9f7a53fbe46c571dc19
Your Name <you@example.com>
2018-07-04 15:01:32 +0200

    Add 404error
    404 error
    Template system
    Removing hardcoded URLs in templates
    Namespacing URL names

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -11,8 +12,10 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+#error if no object
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
