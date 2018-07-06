e7a49b8925432c98a7ca00cb23d412600374847a
TheCM <heniuzpieca@gmail.com>
2018-07-06 19:07:42 +0200

    Add basic view to the polls app

--- polls/urls.py
+++ polls/urls.py
@@ -1,1 +1,7 @@
-# Add your urls here
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+               path('', views.index, name='index'),
+               ]
--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,6 @@
-# Add your views here
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
