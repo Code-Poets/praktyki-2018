ea82bdf20a7a0bd3631253beac1b1a8465bff71b
Karrp <kogut0000@gmail.com>
2018-07-09 14:26:33 +0200

    Error 404 using exception

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -21,3 +22,10 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
