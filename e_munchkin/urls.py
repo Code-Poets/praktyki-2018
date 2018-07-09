f6a80f301b99391f78cdb28d048d7605a39e6e0f
Your Name <you@example.com>
2018-07-09 10:45:06 +0200

    Add signup page
    Add signup urlpatterns in urls.py
    Add new signup html file
    Add href for signup in base.html file

--- e_munchkin/urls.py
+++ e_munchkin/urls.py
@@ -18,10 +18,12 @@ from django.urls import path
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from e_munchkin import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
