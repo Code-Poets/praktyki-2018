0732a6c9d4788cd01eb432a04529057b4cbb660c
Karrp <kogut0000@gmail.com>
2018-07-09 14:28:08 +0200

    Error 404 using get_object_or_404

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.http import Http404
-from django.shortcuts import render
+#from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -24,8 +24,9 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    #try:
+    #    question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
