0d844bdeee36192a7c2674eac81ef7d177b780a4
Maciej <sam.maciej@interia.pl>
2018-07-10 14:01:38 +0200

    Add new game app

--- e_munchkin/settings/defaults.py
+++ e_munchkin/settings/defaults.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'games.apps.GamesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
