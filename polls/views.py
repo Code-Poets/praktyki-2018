df7dc7d833472e4ed864c598e7d8d88c8a606994
Karrp <kogut0000@gmail.com>
2018-07-10 14:43:35 +0200

    Don't show future questions

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,26 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-#from django.http import Http404
+# from django.template import loader
+# from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
