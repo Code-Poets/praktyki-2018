32a701a675f2cb64ea11f7d3f45eb1fbf5eeb2ad
Your Name <you@example.com>
2018-07-11 12:38:08 +0200

    Add test for users login

--- users/test_login.py
+++ users/test_login.py
@@ -1,17 +1,20 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import CustomUser
+
 
 class LogInTest(TestCase):
     def setUp(self):
         self.credentials = {
             'username': 'nobodyuser',
             'password': 'nobodypassword'}
-        User.objects.create_user(**self.credentials)
+        CustomUser.objects.create_user(**self.credentials)
 
     def test_login(self):
-        response = self.client.post('/login/', self.credentials, follow=True)
+        response = self.client.post(reverse("login"), self.credentials, follow=True)
         # should be logged in now
         self.assertTrue(response.context['user'].is_active)
+
 
 class NoUserLogInTest(TestCase):
     def setUp(self):
@@ -21,24 +24,34 @@ class NoUserLogInTest(TestCase):
         # not created user
 
     def test_no_user(self):
-        response = self.client.post('/login/', self.credentials, follow=True)
+        response = self.client.post(reverse("login"), self.credentials, follow=True)
         # should NOT be logged in now
         self.assertFalse(response.context['user'].is_active)
+
 
 class WrongLogInTest(TestCase):
     def setUp(self):
         self.credentials = {
             'username': 'nobodyuser',
             'password': 'nobodypassword'}
-        User.objects.create_user(**self.credentials)
+        CustomUser.objects.create_user(**self.credentials)
 
     def test_wrong_login(self):
-        response = self.client.post('/login/', 'wronguser', 'nobodypassword', follow=True)
+        response = self.client.post(reverse("login"), 'wronguser', 'nobodypassword', follow=True)
         # should NOT be logged in now
         self.assertFalse(response.context['user'].is_active)
 
     def test_wrong_passw(self):
-        response = self.client.post('/login/', 'nobodyuser', 'wrongpassword', follow=True)
+        response = self.client.post(reverse("login"), 'nobodyuser', 'wrongpassword', follow=True)
         # should NOT be logged in now
         self.assertFalse(response.context['user'].is_active)
 
+    def test_no_login(self):
+        response = self.client.post(reverse("login"), '', 'nobodypassword', follow=True)
+        # should NOT be logged in now
+        self.assertFalse(response.context['user'].is_active)
+
+    def test_no_passw(self):
+        response = self.client.post(reverse("login"), 'nobodyuser', '', follow=True)
+        # should NOT be logged in now
+        self.assertFalse(response.context['user'].is_active)
