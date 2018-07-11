a316d6b62fcd6228b33fd7180e59abc510a5ef57
Maciej <sam.maciej@interia.pl>
2018-07-11 17:19:15 +0200

    Add games urlconf

---
+++ games/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'games'
+urlpatterns = [
+               #path('', views.IndexView.as_view(), name='index'),
+                path('gamer/<int:pk>', views.GamerView.as_view(), name='gamer'),
+                path('access/', views.GameAccessView.as_view(), name='game_access'),
+                path('<gamepass>/<nick>/join_game/', views.join_game, name='join_game')
+               #path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+               #path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+               #path('<int:question_id>/vote/', views.vote, name='vote'),
+               ]
