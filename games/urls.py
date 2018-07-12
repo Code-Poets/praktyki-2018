4cb041b2e2a2937c7a989d982b7c6436bb7a43f2
Maciej <sam.maciej@interia.pl>
2018-07-12 14:02:05 +0200

    Add join/resume game feature for player
    Added join_game view which redirects user to game with code given in
    GameAccessView.
    
    The view searches for game containing password provided by
    GameAccessView form. If such game exists, the view searches for existing
    players to check if player had already joined before. If not, the player
    is created and added to game with default stats, only if the player
    number doesn't exceed the given global maximum number of players per
    game, which is defined in game.models.MAX_PLAYERS.

--- games/urls.py
+++ games/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 app_name = 'games'
 urlpatterns = [
-               #path('', views.IndexView.as_view(), name='index'),
-                path('gamer/<int:pk>', views.GamerView.as_view(), name='gamer'),
-                path('access/', views.GameAccessView.as_view(), name='game_access'),
-                path('<gamepass>/<nick>/join_game/', views.join_game, name='join_game')
-               #path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-               #path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-               #path('<int:question_id>/vote/', views.vote, name='vote'),
-               ]
+    # path('', views.IndexView.as_view(), name='index'),
+    path('gamer/<int:pk>', views.GamerView.as_view(), name='gamer'),
+    path('access/', views.GameAccessView.as_view(), name='game_access'),
+    path('<gamepass>/<nick>/join_game/', views.join_game, name='join_game'),
+    path('gamer/<int:gamer_id>/update', views.update_stats, name='update_stats'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+]
