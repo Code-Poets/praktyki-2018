9020214097fe568710ea0c0f021da233a02d1217
Your Name <you@example.com>
2018-07-13 15:45:00 +0200

    Add views for join and create game
    Add classes JoinIn and Create in users/views.py
    Change urlpatterns in urls.py and users/urls.py
    Modify templates: base.html, home.html and join_game.html
    Add extends with base.html in create_game.html

--- e_munchkin/urls.py
+++ e_munchkin/urls.py
@@ -15,15 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
-from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
-from e_munchkin import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
