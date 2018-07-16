5599d9f6e7cb28ced9ea83738c841fd3955025c6
Karrp <kogut0000@gmail.com>
2018-07-16 15:46:14 +0200

    Show gamers in game edit view

--- games/admin.py
+++ games/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 
 from .models import Game, Gamer, CharacterClass, CharacterRace
 
-admin.site.register(Game)
+
+class GamerInline(admin.TabularInline):
+    model = Gamer
+    extra = 1
+
+
+class GameAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['password']}),
+    ]
+    inlines = [GamerInline]
+    list_display = ('password', 'created_at', 'finished_at')
+    list_filter = ['created_at']
+    search_fields = ['password']
+
+
+admin.site.register(Game, GameAdmin)
 admin.site.register(Gamer)
 admin.site.register(CharacterClass)
 admin.site.register(CharacterRace)
