7f22e50bcd77b750d532a6887f754f59a762dc4f
Your Name <you@example.com>
2018-07-18 08:28:58 +0200

    Create game panel bud

--- games/urls.py
+++ games/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:pk>/', views.GamePanelView.as_view(), name='game_panel'),
 ]
