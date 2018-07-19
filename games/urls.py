5c39840fbd31aa506e95825535f7c7ab40c90024
Karrp <kogut0000@gmail.com>
2018-07-19 12:33:26 +0200

    Clean up games/urls.py

--- games/urls.py
+++ games/urls.py
@@ -4,16 +4,22 @@ from . import views
 
 app_name = 'games'
 urlpatterns = [
+    # ex: /games/
     # path('', views.IndexView.as_view(), name='index'),
-    path('gamer/<int:pk>', views.GamerView.as_view(), name='gamer'),
-    path('access/', views.GameAccessView.as_view(), name='game_access'),
-    path('<gamepass>/<nick>/join_game/', views.join_game, name='join_game'),
+    
+    # ex: /games/create/
     path('create/', views.CreateGameView.as_view(), name='create_game'),
-    path('<int:pk>/edit/', views.EditGameView.as_view(), name='edit_game'),
-    path('gamer/<int:pk>/leave', views.DeleteGamer.as_view(), name='delete_gamer'),
-    # path('gamer/<int:gamer_id>/update', views.update_stats, name='update_stats'),
-    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # ex: /games/1
     path('<int:pk>/', views.game_panel_view, name='game_panel'),
+    # ex: /games/1/edit
+    path('<int:pk>/edit/', views.EditGameView.as_view(), name='edit_game'),
+
+    # ex: /games/access/
+    path('access/', views.GameAccessView.as_view(), name='game_access'),
+    # ex: /games/ABCD/Humbert/join_game/
+    path('<gamepass>/<nick>/join_game/', views.join_game, name='join_game'),
+    # ex: /games/gamer/5/
+    path('gamer/<int:pk>', views.GamerView.as_view(), name='gamer'),
+    # ex: /games/gamer/5/lave
+    path('gamer/<int:pk>/leave', views.DeleteGamer.as_view(), name='delete_gamer'),
 ]
