4d9c079b4d60cb4439548214ab715e999f766a7e
Karrp <kogut0000@gmail.com>
2018-07-26 10:00:11 +0200

    Remove access permissions to delete gamer (leave game) by someone else

--- games/views.py
+++ games/views.py
@@ -335,6 +335,24 @@ class DeleteGamerView(generic.DeleteView):
     template_name = 'games/delete_gamer.html'
     success_url = '/'
 
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().game.finished_at is not None:
+            raise Http404("No gamer found")
+        else:
+            if self.request.user.is_authenticated:
+                if self.get_object().user != self.request.user:
+                    raise Http404("No gamer found")
+            else:
+                # if 'gamer_id' not in request.session:
+                gamer_id = request.session.get('gamer_id', None)
+                if gamer_id is None:
+                    raise Http404("No gamer found")
+                else:
+                    # gamer_id = request.session['gamer_id']
+                    if self.get_object().id != request.session['gamer_id']:
+                        raise Http404("No gamer found")
+        return super(DeleteGamerView, self).dispatch(request, *args, **kwargs)
+
     def delete(self, request, *args, **kwargs):
         request.session['gamer_id'] = None
         return super(DeleteGamerView, self).delete(request, *args, **kwargs)
