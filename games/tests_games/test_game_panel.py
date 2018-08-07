f8ad3dc546fba38ff8a4741ca4a20e62cd649ec9
Karrp <kogut0000@gmail.com>
2018-08-07 13:49:34 +0200

    Move game tests to new directory

--- games/tests_games/test_game_panel.py
+++ games/tests_games/test_game_panel.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import Game, CustomUser
+from games.models import Game, CustomUser
 
 # Create your tests here.
 
+
 class GamePanel(TestCase):
-   def setUp(self):
+    def setUp(self):
         self.credentials = {
             'username': 'nobodyuser',
             'password': 'nobodypassword'}
@@ -19,9 +20,9 @@ class GamePanel(TestCase):
             'winning_level': 10}
         self.game = Game.objects.create(**self.credentials)
 
-   def test_index_page(self):
-       url = reverse('games:game_panel', args=(self.game.id,))
-       response = self.client.get(url)
-       self.assertEqual(response.status_code, 200)
-       self.assertTemplateUsed(response, 'games/game_panel.html')
-       self.assertContains(response, 'Welcome to: </font>NewGameTest')
+    def test_index_page(self):
+        url = reverse('games:game_panel', args=(self.game.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'games/game_panel.html')
+        self.assertContains(response, 'Welcome to: </font>NewGameTest')
