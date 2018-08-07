d92a551770cf291b8e36ab59d11c50b3ae99694c
Your Name <you@example.com>
2018-08-07 17:30:55 +0200

    Remove unused form from users/form.py

--- users/forms.py
+++ users/forms.py
@@ -24,42 +24,4 @@ class CustomUserChangeForm(UserChangeForm):
         password2 = self.cleaned_data.get('password2', None)
         return self.cleaned_data
 
-"""
-class CustomUserEditForm(CustomUserChangeForm):
-    username = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    nick = forms.CharField(required=True)
-
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'nick')
-
-    def clean_email(self):
-        username = self.cleaned_data.get('username')
-        email = self.cleaned_data.get('email')
-
-        if email and CustomUser.objects.filter(email=email).exclude(username=username).count():
-            raise forms.ValidationError('This email address is already in use. Please supply a different email address.')
-        return email
-
-    def save(self, commit=True):
-        user = super(CustomUserChangeForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-
-        if commit:
-            user.save()
-        return user
-
-    def clean_password(self):
-        password1 = self.cleaned_data.get('password1', None)
-        password2 = self.cleaned_data.get('password2', None)
-        return self.cleaned_data
-"""
-"""
-        if password1 and password2 and (password1 == password2):
-            return self.cleaned_data
-        else:
-            raise forms.ValidationError("Passwords don't match")
-"""
-
 
