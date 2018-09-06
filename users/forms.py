9317a8089c790840a0a9ebfd96456678396b98d8
Maciej <sam.maciej@interia.pl>
2018-09-06 17:04:43 +0200

    Translate user forms

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import ugettext_lazy as _
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
 
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput, help_text="Your password must contain at least 8 characters.")
-    password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput, help_text="Enter the same password as above, for verification.")
+    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput, help_text=_("Your password must contain at least 8 characters."))
+    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput, help_text=_("Enter the same password as above, for verification."))
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
@@ -27,5 +28,3 @@ class CustomUserChangeForm(UserChangeForm):
         password1 = self.cleaned_data.get('password1', None)
         password2 = self.cleaned_data.get('password2', None)
         return self.cleaned_data
-
-
