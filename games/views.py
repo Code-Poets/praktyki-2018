4bff891399c23a4285ce24893b9e5250f3054a33
Martyna <martyna.gottschling@gmail.com>
2018-09-06 13:19:26 +0200

    Add gamer order by 'order' in game edit view

--- games/views.py
+++ games/views.py
@@ -184,6 +184,11 @@ class EditGameView(generic.UpdateView):
     def get_queryset(self):
         return Game.objects
 
+    def get_context_data(self, **kwargs):
+        context = super(EditGameView, self).get_context_data(**kwargs)
+        context['gamers'] = Gamer.objects.filter(game__id=self.kwargs['pk']).order_by('order')
+        return context
+
     def form_valid(self, form):
         form.save()
         return HttpResponseRedirect(reverse('games:edit_game', args=(form.instance.id,)))
